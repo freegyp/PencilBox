4bac831cd7607b75189b1ca727365dab0262ac57
freegyp <admin@gypmac.local>
2016-04-09 20:40:13 -0700

    Initial Commit

---
+++ CampusLite/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CampusLite
+//
+//  Created by gyp on 4/9/16.
+//  Copyright © 2016 gyp. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
